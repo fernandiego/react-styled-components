b4d37e5a914364bca98eed965e956bf4c203a2e3
fox <fernandodiego.sn@gmail.com>
2023-08-03 20:56:29 -0300

    Inversão de tema por clique de botão

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import { ThemeProvider } from "styled-components";
 import Cabecalho from "./Components/Cabecalho";
 import Container from "./Components/Container";
 import { GlobalStyle } from "./Components/GlobalStyle";
-import { temaClaro, temaEscuro } from "./Components/UI/temas";
+import { BtnTema, temaClaro, temaEscuro } from "./Components/UI/temas";
+import SwitchTheme from "./Components/SwitchTheme";
 
 function App() {
+  const [tema, setTema] = useState(true)
+  const toggleTheme = () => {
+    setTema((tema) => !tema)
+  }
   return (
-    <ThemeProvider theme={temaEscuro}>
+    <ThemeProvider theme={tema ? temaClaro : temaEscuro}>
 
       <GlobalStyle />
+      <BtnTema onClick={toggleTheme}>
+        <SwitchTheme/>
+      </BtnTema>
       <Cabecalho />
       <Container />
     </ThemeProvider>
