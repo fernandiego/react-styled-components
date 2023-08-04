7b0a1e01fa1543fdd0f120e1b1ed9c563246e6a4
fox <fernandodiego.sn@gmail.com>
2023-08-03 21:02:39 -0300

    Inversão de tema, aplicando aos ícones também

--- src/Components/UI/temas.js
+++ src/Components/UI/temas.js
@@ -12,21 +12,23 @@ import styled from "styled-components";
 export const temaClaro = {
   body: fundoClaro,
   inside: conteudoClaro,
-  text: textoFundoClaro
-}
+  text: textoFundoClaro,
+  filter: ""
+};
 
 export const temaEscuro = {
   body: fundoEscuro,
   inside: conteudoEscuro,
-  text: textoFundoEscuro
-}
+  text: textoFundoEscuro,
+  filter: "invert(100%)"
+};
 
 export const BtnTema = styled.button`
-position: absolute;
+  position: absolute;
   top: 4vh;
   right: 20px;
   background-color: inherit;
   border: none;
   cursor: pointer;
-  
-`
+
+`;
